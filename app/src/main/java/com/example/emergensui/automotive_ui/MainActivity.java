c0bc08adbfe75dbb3b13a9524f6b0062563c3e1b
Lucas <lqquyen12894@gmail.com>
2019-10-15 01:59:59 -0400

    Splash Screen Added

--- app/src/main/java/com/example/emergensui/automotive_ui/MainActivity.java
+++ app/src/main/java/com/example/emergensui/automotive_ui/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.emergensui.automotive_ui;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int SPLASH_DELAY = 5000; //Delays for 5 seconds
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent in = new Intent(MainActivity.this, splash_screen.class);
+                startActivity(in);
+                finish();
+            }
+        }, SPLASH_DELAY);
     }
 }
