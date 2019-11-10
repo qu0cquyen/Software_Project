5dabcb03c0b66e9f3abde2447a3af1a5cb3efc04
SeungMin-Song <54861193+SeungMin-Song@users.noreply.github.com>
2019-11-10 17:11:33 -0500

    Add test graph code

--- app/src/main/java/com/example/emergensui/automotive_ui/paramedic_screen.java
+++ app/src/main/java/com/example/emergensui/automotive_ui/paramedic_screen.java
@@ -6,8 +6,17 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class paramedic_screen extends AppCompatActivity {
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.Random;
+
+public class paramedic_screen extends AppCompatActivity {
+    private LineGraphSeries<DataPoint> series;
+    private static final Random RAMDOM = new Random();
+    private int lastX = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +32,42 @@ public class paramedic_screen extends AppCompatActivity {
 
         });
 
+        //Create real time graph
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>();
+        graph.addSeries(series);
+        Viewport viewport = graph.getViewport();
+        viewport.setYAxisBoundsManual(true);
+        viewport.setMinY(0);
+        viewport.setMaxY(10);
+        viewport.setScrollable(true);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new Thread(new Runnable(){
+            @Override
+            public void run(){
+                for(int i=0;;i++){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            addEntry();
+                        }
+                    });
+                    try {
+                        Thread.sleep(600);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        }).start();
+    }
+    // add random data to graph
+    private void addEntry(){
+        series.appendData(new DataPoint(lastX++, RAMDOM.nextDouble()*10d),true,10);
     }
 }
