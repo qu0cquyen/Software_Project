2deb26e9cd0917ff3cf48ab29f742a1b6c65c50c
Lucas <lqquyen12894@gmail.com>
2019-11-14 15:43:34 -0500

    Loading database on nested Recycler View

--- app/src/main/java/com/example/emergensui/automotive_ui/Adapter/PatientAdapter.java
+++ app/src/main/java/com/example/emergensui/automotive_ui/Adapter/PatientAdapter.java
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class PatientAdapter extends RecyclerView.Adapter<PatientAdapter.PatientHolder>
@@ -21,12 +23,17 @@ public class PatientAdapter extends RecyclerView.Adapter<PatientAdapter.PatientH
     private Context context;
     private List<Patient> lstPatient;
 
-    private boolean visible = true;
+    private RecyclerView.RecycledViewPool recycledViewPool;
+
+    private boolean visible = false;
+    private int pos;
 
     public PatientAdapter(Context context, ArrayList<Patient> lstp)
     {
         this.context = context;
         this.lstPatient = lstp;
+
+        recycledViewPool = new RecyclerView.RecycledViewPool();
     }
 
     @NonNull
@@ -38,12 +45,33 @@ public class PatientAdapter extends RecyclerView.Adapter<PatientAdapter.PatientH
 
     @Override
     public void onBindViewHolder(@NonNull PatientHolder holder, int position) {
-        Patient p = lstPatient.get(position);
+        final Patient p = lstPatient.get(position);
         holder.setDetails(p);
 
+        int poss = p.getPos();
+
+        List<Patient> lstpp = new ArrayList<>();
+        lstpp.add(p);
+
+        PatientChildAdapter pca = new PatientChildAdapter(lstpp, context);
+
+        holder.childRecycler.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false));
+        holder.childRecycler.setAdapter(pca);
 
 
 
+
+
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(visible){visible = false;}
+                else {visible = true;}
+                notifyItemChanged(p.getPos());
+            }
+
+        });
 
 
     }
@@ -57,25 +85,34 @@ public class PatientAdapter extends RecyclerView.Adapter<PatientAdapter.PatientH
     {
 
         private TextView txtPatientName;
-        private TextView txtPatientRoom;
+        /*private TextView txtPatientRoom;
         private TextView txtPatientType;
-        private TextView txtPatientDoB;
+        private TextView txtPatientDoB;*/
+        private View subItems;
+        private RecyclerView childRecycler;
 
         PatientHolder(@NonNull View itemView)
         {
             super(itemView);
             txtPatientName = itemView.findViewById(R.id.lbPatientName);
-            txtPatientRoom = itemView.findViewById(R.id.lblPatientRoom);
+            /*txtPatientRoom = itemView.findViewById(R.id.lblPatientRoom);
             txtPatientType = itemView.findViewById(R.id.lblPatientTypeValue);
-            txtPatientDoB = itemView.findViewById(R.id.lblPatientDoB_Value);
+            txtPatientDoB = itemView.findViewById(R.id.lblPatientDoB_Value);*/
+            //subItems = itemView.findViewById(R.id.sub_items);
+            childRecycler = itemView.findViewById(R.id.sub_items);
+            childRecycler.addItemDecoration(new DividerItemDecoration(context, LinearLayoutManager.VERTICAL));
         }
 
         void setDetails(Patient p)
         {
             txtPatientName.setText(p.getPatient_Name());
-            txtPatientRoom.setText(p.getRoom_Number());
+            /*txtPatientRoom.setText(p.getRoom_Number());
             txtPatientType.setText(p.getPatient_Type());
-            txtPatientDoB.setText(p.getDoB());
+            txtPatientDoB.setText(p.getDoB());*/
+
+            childRecycler.setVisibility(visible? View.VISIBLE : View.GONE);
+
+
 
         }
     }
--- app/src/main/java/com/example/emergensui/automotive_ui/Class/Patient.java
+++ app/src/main/java/com/example/emergensui/automotive_ui/Class/Patient.java
@@ -1,10 +1,15 @@
 package com.example.emergensui.automotive_ui.Class;
 
+import java.util.List;
+
 public class Patient {
     private String DoB;
     private String Patient_Type;
     private String Room_Number;
     private String Patient_Name;
+    private int pos;
+
+    private List<Patient> lstPatient;
 
     public Patient(){}
 
@@ -15,6 +20,7 @@ public class Patient {
         this.Room_Number = room_number;
         this.Patient_Name = Name;
     }
+
 
     public void setDoB(String dob)
     {
@@ -50,5 +56,21 @@ public class Patient {
 
     public String getPatient_Name(){return Patient_Name;}
 
+    public void setLstPatient(List<Patient> lst)
+    {
+        this.lstPatient = lst;
+    }
 
+    public List<Patient> getLstPatient()
+    {
+        return this.lstPatient;
+    }
+
+    public int getPos() {
+        return pos;
+    }
+
+    public void setPos(int pos) {
+        this.pos = pos;
+    }
 }
